cf50f3086aa1a6c689cb73ea95d2046c519f40b2
Germano Macieira <germanomacieira@hotmail.com>
2022-04-22 09:27:56 -0300

    Acrescentando comentários ao DTO

--- src/main/java/br/unifor/dtec/contatos/model/dto/ContatoDto.java
+++ src/main/java/br/unifor/dtec/contatos/model/dto/ContatoDto.java
@@ -1,7 +1,14 @@
 package br.unifor.dtec.contatos.model.dto;
 
 import br.unifor.dtec.contatos.model.Contato;
-
+/* DTO = data transfer object;
+ * OBS: Não é uma boa prática devolver os dados das entidades/modelos (classe de domínio);
+ * Geralmente, controi-se uma classe secundária(DTO) para retornar as informações apropriadas e com mais segurança.
+ * Por exemplo, caso se tenha uma entidade contendo dados sensíveis, como senha, cpf, etc, não se retorna esses dados!
+ * No exemplo abaixo, foi preservado o número do cpf;
+ * No construtor, observa-se que a classe dto recebe como parâmetro a entidade inteira, porém escolhe-se somente os 
+ * atributos desejados;
+ */
 public class ContatoDto {
 	
 	// ATRIBUTOS:
